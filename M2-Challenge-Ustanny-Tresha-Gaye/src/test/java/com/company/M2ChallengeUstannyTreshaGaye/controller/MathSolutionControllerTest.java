a821b3a4419c8ebc3bed2a5f9f721541fa61dfa1
Twigster <treshagaye@gmail.com>
2022-07-08 12:15:11 -0400

    started tests

--- M2-Challenge-Ustanny-Tresha-Gaye/src/test/java/com/company/M2ChallengeUstannyTreshaGaye/controller/MathSolutionControllerTest.java
+++ M2-Challenge-Ustanny-Tresha-Gaye/src/test/java/com/company/M2ChallengeUstannyTreshaGaye/controller/MathSolutionControllerTest.java
@@ -1,7 +1,25 @@
 package com.company.M2ChallengeUstannyTreshaGaye.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(MathSolutionController.class)
 public class MathSolutionControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+
 
 }
